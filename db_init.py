655c8d85cdb8067e9a4fd1c61b6ddcb6a0fd3ff8
Stephen <stevobialor@gmail.com~>
2024-04-22 23:19:36 +0100

    modify

--- db_init.py
+++ db_init.py
@@ -11,29 +11,11 @@ from api.v1.models.order import Order
 from api.v1.models.buyer import Buyer
 
 
-# Photos
-ph1 = Photo()
-ph1.url = 'https://my_photos.com'
-ph1.name = 'Ebube profile'
-ph1.save()
-
-ph2 = Photo()
-ph2.url = 'https://my_photos.com'
-ph2.name = 'Ada profile'
-ph2.save()
-
-ph3 = Photo()
-ph3.url = 'https://my_photos.com'
-ph3.name = 'Stethoscope picture'
-ph3.save()
-
-
 # Vendors
 v1 = Vendor(password='pwd')
 v1.email = 'ebube@gmail.com'
 v1.first_name = 'Ebube'
 v1.last_name = 'Onwuta'
-v1.photo_id = ph1.id
 v1.is_male = True
 v1.department = 'Medicine and surgery'
 v1.level = 200
@@ -55,7 +37,6 @@ p1.price = 50 * 1000 * 100  # In kobo (NGN)
 p1.qty = 7
 p1.category_id = c1.id
 p1.vendor_id = v1.id
-p1.photo_id = ph3.id
 p1.save()
 
 
@@ -65,7 +46,6 @@ b1.email = 'ada@gmail.com'
 b1.first_name = 'Ada'
 b1.last_name = 'Zion'
 b1.is_male = False
-b1.photo_id = ph2.id
 b1.location = 'Mountain Zion'
 b1.save()
 
@@ -85,13 +65,5 @@ ord1.buyer_id = b1.id
 ord1.qty = p1.qty - 1
 ord1.status = 'pending'
 ord1.notes = 'I need it as soon as possible'
-ord1.delivery_date = datetime.utcnow() + timedelta(days=5)
+ord1.delivery_date = datetime.now() + timedelta(days=5)
 ord1.save()
-
-
-# Payments
-pay1 = Payment()
-pay1.order_id = ord1.id
-pay1.amt = (50 * 1000 * 100) + 3000
-pay1.transaction_id = 12344     # Obtained from PayStack
-pay1.save()
